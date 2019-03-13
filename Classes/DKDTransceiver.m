375a4d15d6510398278deb90840d1cbb418c1539
Moky <albert.moky@gmail.com>
2019-03-13 19:06:34 +0800

    should not ignore the message sending to the sender itself

--- Classes/DKDTransceiver.m
+++ Classes/DKDTransceiver.m
@@ -66,10 +66,6 @@ SingletonImplementations(DKDTransceiver, sharedInstance)
         }
         BOOL OK = YES;
         for (rMsg in messages) {
-            if ([rMsg.envelope.receiver isEqual:rMsg.envelope.sender]) {
-                // ignore message to the sender itself
-                continue;
-            }
             if ([self sendReliableMessage:rMsg callback:callback]) {
                 //NSLog(@"group message sent to %@", rMsg.envelope.receiver);
             } else {
