9243c8342e3da741347254fc8c081aff205d5afe
Moky <albert.moky@gmail.com>
2019-12-02 19:31:51 +0800

    SDK test OK

--- Classes/DKDMessage+Transform.h
+++ Classes/DKDMessage+Transform.h
@@ -1,3 +1,32 @@
+// license: https://mit-license.org
+//
+//  Dao-Ke-Dao: Universal Message Module
+//
+//                               Written in 2019 by Moky <albert.moky@gmail.com>
+//
+// =============================================================================
+// The MIT License (MIT)
+//
+// Copyright (c) 2019 Albert Moky
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
 //
 //  DKDMessage+Transform.h
 //  DaoKeDao
@@ -12,7 +41,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/**
+/*
  *  Message Transforming
  *  ~~~~~~~~~~~~~~~~~~~~
  *
@@ -185,7 +214,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 #pragma mark - Transform
 
-/**
+/*
  *  Encrypt the Instant Message to Secure Message
  *
  *    +----------+      +----------+
@@ -208,7 +237,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-/**
+/*
  *  Decrypt the Secure Message to Instant Message
  *
  *    +----------+      +----------+
@@ -226,7 +255,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-/**
+/*
  *  Sign the Secure Message to Reliable Message
  *
  *    +----------+      +----------+
@@ -245,7 +274,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-/**
+/*
  *  Verify the Reliable Message to Secure Message
  *
  *    +----------+      +----------+
